3d41c14615e7a42f676f5acd00b8e047a6727449
SuperPox <calebkohl@gmail.com>
2021-04-23 06:51:29 -0600

    commit tree 50fae29389a802f2d8a32950e6e6961a76cc16b5

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// console.log(couponLocations.reduce(couponCounter, 0)); // also logs 15!
+
+const totalBatteries = batteryBatches.reduce(function(all, batches)
+{
+    return all + batches
+})
